732ee81d2ad978553889a2cb621565997fb6fbd5
BanTanger <88583317+BanTanger@users.noreply.github.com>
2023-03-06 21:36:56 +0800

    第一章：创建简单的映射器代理工厂

---
+++ src/main/java/com/bantanger/mybatis/binding/MapperProxy.java
@@ -0,0 +1,34 @@
+package com.bantanger.mybatis.binding;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * 映射器代理类
+ * @author BanTanger 半糖
+ * @Date 2023/3/6 20:09
+ */
+public class MapperProxy<T> implements InvocationHandler, Serializable {
+
+    private static final long serialVersionUID = -6424540398559729838L;
+
+    private Map<String, String> sqlSession;
+    private final Class<T> mapperInterface;
+
+    public MapperProxy(Map<String, String> sqlSession, Class<T> mapperInterface) {
+        this.sqlSession = sqlSession;
+        this.mapperInterface = mapperInterface;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (Object.class.equals(method.getDeclaringClass())) {
+            return method.invoke(this, args);
+        } else {
+            return "你被代理了！" + sqlSession.get(mapperInterface.getName() + "." + method.getName());
+        }
+    }
+
+}
---
+++ src/test/java/com/bantanger/mybatis/test/dao/IUserDao.java
@@ -0,0 +1,13 @@
+package com.bantanger.mybatis.test.dao;
+
+/**
+ * @author BanTanger 半糖
+ * @Date 2023/3/6 21:00
+ */
+public interface IUserDao {
+
+    String queryUserName(String uId);
+
+    Integer queryUserAge(String uId);
+
+}
